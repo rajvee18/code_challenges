b2191134820aa20cb8a327164eb1677348252b32
rajvee18 <rajvee23@iisertvm.ac.in>
2024-09-12 23:58:31 +0530

    capitalise

--- capitalise!/capitlaise.py
+++
@@ -1,8 +0,0 @@
-s = input()
-
-def solve(s):
-    n1,n2 = s.strip(' ')
-
-    print(n1,n2)
-
-solve()
---
+++ capitalise/capitlaise.py
@@ -0,0 +1,14 @@
+s = input()
+
+def solve(s):
+    n1 = s.split(' ')
+
+    
+
+    
+
+    name = ' '.join(i.captalize() for i in n1)
+
+    print(name)
+
+solve(s)
