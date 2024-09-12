ab336a837296fea0cd03d0ef113bd966b82318f6
rajvee18 <rajvee23@iisertvm.ac.in>
2024-09-13 00:05:44 +0530

    capitalise

--- capitalise/capitlaise.py
+++ capitalise/capitlaise.py
@@ -2,13 +2,7 @@ s = input()
 
 def solve(s):
     n1 = s.split(' ')
-
-    
-
-    
-
-    name = ' '.join(i.captalize() for i in n1)
-
+    name = ' '.join(i.capitalize() for i in n1)
     print(name)
 
 solve(s)
