e3b03d73d946d28ac8d521121b31274c943a0c01
rajvee18 <rajvee23@iisertvm.ac.in>
2024-09-14 19:08:40 +0530

    minion_game

--- the_minion_game/minion_game.py
+++ the_minion_game/minion_game.py
@@ -1,10 +1,77 @@
-s = input()
+s = input().lower()
 
 vowels = ['a','e','i','o','u']
 
 Stuart = 0
 Kevin = 0
+end = len(s)
 
-#for i in range(0,len(s)):
-    #if s[i] in vowels:
+def CountOccurrences(string, substring):
+ 
+    # Initialize count and start to 0
+    count = 0
+    start = 0
+ 
+    # Search through the string till
+    # we reach the end of it
+    while start < len(string):
+ 
+        # Check if a substring is present from
+        # 'start' position till the end
+        pos = string.find(substring, start)
+ 
+        if pos != -1:
+            # If a substring is present, move 'start' to
+            # the next position from start of the substring
+            start = pos + 1
+ 
+            # Increment the count
+            count += 1
+        else:
+            # If no further substring is present
+            break
+    # return the value of count
+    return count
+
+
+i=0
+
+while i < end: 
+    if s[i] in vowels:
+        for j in range(i,end):
+            substring = s[i:j+1]
+            x = CountOccurrences(s,substring)
+            Kevin = Kevin + x
+        
+    i = i+1
+
+
+
+i=0
+while i < end:
+    if s[i] not in vowels:
+        for j in range(i,end):
+            substring = s[i:j+1]
+            x = CountOccurrences(s,substring)
+            Stuart = Stuart + x
+        
+    i = i+1   
+
+
+    
+    
+    
+
+
+print(Stuart,Kevin)
+
+
+
+
+
+
+
+
+
+
         
