a19e5e44f77a005f44569e14373e2fe9aebfbc6c
rajvee18 <rajvee23@iisertvm.ac.in>
2024-09-15 14:54:04 +0530

    alternate_minion_game

---
+++ the_minion_game/alternate_minion_game.py
@@ -0,0 +1,23 @@
+def minion_game(s):
+    vowels = 'AEIOU'
+    Stuart = 0
+    Kevin = 0
+    end = len(s)
+
+    for i in range(end):
+        if s[i] in vowels:
+            Kevin += (end - i)
+        else:
+            Stuart += (end - i)
+
+    if Stuart > Kevin:
+        print("Stuart", Stuart)
+    elif Kevin > Stuart:
+        print("Kevin", Kevin)
+    else:
+        print("Draw")
+
+# Example usage
+if __name__ == "__main__":
+    s = input().strip()
+    minion_game(s)
