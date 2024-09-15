858149e75e7fcedfde14c5020df97b3a2512bb84
rajvee18 <rajvee23@iisertvm.ac.in>
2024-09-15 14:42:35 +0530

    New branch

--- the_minion_game/minion_game.py
+++ the_minion_game/minion_game.py
@@ -1,61 +1,27 @@
-s = input().lower()
+def minion_game(s):
+    vowels = 'AEIOU'
+    Stuart = 0
+    Kevin = 0
+    end = len(s)
 
-vowels = ['a','e','i','o','u']
-
-Stuart = 0
-Kevin = 0
-end = len(s)
-
-def CountOccurrences(string, substring):
- 
-    # Initialize count and start to 0
-    count = 0
-    start = 0
- 
-    # Search through the string till
-    # we reach the end of it
-    while start < len(string):
- 
-        # Check if a substring is present from
-        # 'start' position till the end
-        pos = string.find(substring, start)
- 
-        if pos != -1:
-            # If a substring is present, move 'start' to
-            # the next position from start of the substring
-            start = pos + 1
- 
-            # Increment the count
-            count += 1
+    for i in range(end):
+        if s[i] in vowels:
+            Kevin += (end - i)
         else:
-            # If no further substring is present
-            break
-    # return the value of count
-    return count
+            Stuart += (end - i)
 
+    if Stuart > Kevin:
+        print("Stuart", Stuart)
+    elif Kevin > Stuart:
+        print("Kevin", Kevin)
+    else:
+        print("Draw")
 
-i=0
-
-while i < end: 
-    if s[i] in vowels:
-        for j in range(i,end):
-            substring = s[i:j+1]
-            x = CountOccurrences(s,substring)
-            Kevin = Kevin + x
-        
-    i = i+1
-
-
-
-i=0
-while i < end:
-    if s[i] not in vowels:
-        for j in range(i,end):
-            substring = s[i:j+1]
-            x = CountOccurrences(s,substring)
-            Stuart = Stuart + x
-        
-    i = i+1   
+# Example usage
+if __name__ == "__main__":
+    s = input().strip()
+    minion_game(s)
+    
 
 
     
@@ -63,7 +29,7 @@ while i < end:
     
 
 
-print(Stuart,Kevin)
+
 
 
 
